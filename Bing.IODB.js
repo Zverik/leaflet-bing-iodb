fc495a2dcf3b0397a2599f19664c45f4cd9603ff
Ilya Zverev <D:\Mail>
2013-09-20 17:44:05 +0400

    initial commit

---
+++ Bing.IODB.js
@@ -0,0 +1,80 @@
+// Written by Ilya Zverev, licensed WTFPL
+
+L.BingLayer = L.BingLayer.extend({
+    options: {
+        offsetServer: 'http://offsets.textual.ru/',
+        offsetUpdateDistance: 1000,
+        offsetEnabled: true
+    },
+
+    setOffsetEnabled: function(e) {
+        if( e != this.options.offsetEnabled ) {
+            this.options.offsetEnabled = e;
+            this._updateContainerOffset();
+        }
+    },
+
+    _update: function() {
+        if (this._url == null || !this._map) return;
+        this._update_attribution();
+        if( !this._offset0 ) this._offset0 = new L.Point(0, 0);
+        this._refreshOffset();
+        this._updateContainerOffset();
+        L.TileLayer.prototype._update.apply(this, []);
+    },
+
+    _refreshOffset: function() {
+        if( this._map.getZoom() < 15 )
+            return;
+        var currentPos = this._map.getCenter();
+        if( this._lastOffsetPos && this._lastOffsetPos.distanceTo(currentPos) < this.options.offsetUpdateDistance )
+            return;
+        this._lastOffsetPos = currentPos;
+        this._queryIODB(currentPos, this, function(ctx, data) {
+            var found = null;
+            for( var i = 0; i < data.length; i++ ) {
+                if( data[i]['type'] == 'offset' && !data[i]['deprecated'] ) {
+                    found = data[i];
+                    break;
+                }
+            }
+            var distance = 0;
+            if( found ) {
+                var from = new L.LatLng(found['lat'], found['lon']);
+                var to = new L.LatLng(found['imlat'], found['imlon']);
+                distance = from.distanceTo(to);
+                ctx._offset = ctx._map.project(to, 22).subtract(ctx._map.project(from, 22));
+            } else
+                ctx._offset = ctx._offset0;
+            ctx._updateContainerOffset();
+            ctx.fire('offset', { distance: distance, found: found });
+        });
+    },
+
+    _updateContainerOffset: function() {
+        if( !this._offset ) return;
+        var style = this._container.style,
+            zoom = this._map.getZoom(),
+            offset = zoom < 15 || !this.options.offsetEnabled ? this._offset0 : this._offset,
+            zoomDiff = Math.pow(2, 22 - zoom);
+        style.left = Math.round(-offset.x / zoomDiff);
+        style.top = Math.round(-offset.y / zoomDiff);
+    },
+
+    _queryIODB: function (latlng, context, callback) {
+        var url = this.options.offsetServer + 'get?format=json&imagery=bing&lat=' + latlng.lat + '&lon=' + latlng.lng;
+        var http = null;
+        if (window.XMLHttpRequest) {
+            http = new XMLHttpRequest();
+        } else if (window.ActiveXObject) { // Older IE.
+            http = new ActiveXObject("MSXML2.XMLHTTP.3.0");
+        }
+        http.onreadystatechange = function() {
+            if( http.readyState != 4 || http.status != 200 ) return;
+            var result = eval(http.responseText);
+            callback(context, result);
+        };
+        http.open('GET', url, true);
+        http.send(null);
+    }
+});
---
+++ README.md
@@ -0,0 +1,20 @@
+# IODB for Bing Leaflet Layer
+
+[Demonstration page](http://zverik.github.io/leaflet-bing-iodb/)
+
+Nice projects like [show-me-the-way](http://osmlab.github.io/show-me-the-way/), that
+show OSM data on top of Bing imagery, have one tiny flaw: in most areas of the world
+Bing imagery is offset by several meters, and mappers usually fix its alignment using
+GPX traces. This results in lines shown being slightly misaligned with imagery.
+This plugin alters [Bing layer](https://github.com/shramov/leaflet-plugins/blob/master/layer/tile/Bing.js)
+using responses from [Imagery Offset Database](http://wiki.openstreetmap.org/wiki/Imagery_Offset_Database).
+So if there is an offset in the database, it is applied to the Bing layer (only on zooms 15 and higher).
+
+Testing showed this plugin makes one request in 4-5 seconds when scrolling, so while
+projected load on IODB server is not high, please do not use this plugin on public
+map displaying websites, like openstreetmap.ru. Show-me-the-way is ok, I guess: not many
+users watch it simultaneously.
+
+Usage is simple: include `Bing.IODB.js` — and you're set.
+
+Written by Ilya Zverev, licensed WTFPL.
